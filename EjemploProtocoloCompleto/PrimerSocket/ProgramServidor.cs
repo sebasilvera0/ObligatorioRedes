96a2482aa5588628b9d8d3f49faf9aac6cd03811
Sebastian Silvera <sebasilvera0@gmail.com>
2023-04-05 20:34:37 -0300

    Comienzo

---
+++ EjemploProtocoloCompleto/PrimerSocket/ProgramServidor.cs
@@ -0,0 +1,66 @@
+﻿using Protocolo;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PrimerSocket
+{
+    internal class ProgramServidor
+    {
+        static int cantClientes = 3;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando Servidor");
+            // 1- Se crea el Socket del servidor
+            var socketServidor = new Socket(
+                AddressFamily.InterNetwork, 
+                SocketType.Stream, 
+                ProtocolType.Tcp);
+
+            // 2- Creamos un EndPoint con Ip = localHost + Puerto
+            var endPointServidor = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
+
+            // 3- Asociamos el socket con el EndPoint por medio del Bind
+            socketServidor.Bind(endPointServidor);
+
+            // 4- Ponemos el socket en modo escucha
+            socketServidor.Listen(10);
+
+            int clientesConectados = 0;
+
+            while (clientesConectados < cantClientes)
+            {
+                // 5- Aceptamos N conexiones
+                var socketCliente = socketServidor.Accept(); //Bloquea el hilo principal del Servidor, queda esperando por una conexión de un Cliente
+                Console.WriteLine("Cliente conectado!");
+                clientesConectados++;
+                // Disparamos un hilo para manejar cada Cliente
+                new Thread(() => ManejoCliente(socketCliente)).Start();
+            }
+            Console.ReadLine();
+        }
+
+        static void ManejoCliente(Socket socketCliente)
+        {
+            bool clienteConectado = true;
+            ManejoDataSocket manejoDataSocket = new ManejoDataSocket(socketCliente);
+            while (clienteConectado)
+            {
+                try
+                {
+                    byte[] datosLargo = manejoDataSocket.Receive(Constantes.LargoFijo);
+                    byte[] datos = manejoDataSocket.Receive(BitConverter.ToInt32(datosLargo));
+                    
+                    string mensaje = $"El cliente dice {Encoding.UTF8.GetString(datos)}";
+                    Console.WriteLine(mensaje);
+                }
+                catch (SocketException e)
+                {
+                    clienteConectado = false;
+                }
+
+            }
+            Console.WriteLine("Cliente desconectado");
+        }
+    }
+}
---
+++ EjemploProtocoloCompleto/Protocolo/ManejoDataSocket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocolo
+{
+    public class ManejoDataSocket
+    {
+        private readonly Socket _socket;
+
+        public ManejoDataSocket(Socket socket)
+        {
+            _socket = socket;
+        }
+
+        public void Send(byte[] buffer) //Los datos ya vienen convertidos a byte[]
+        {
+            int offset = 0;
+            int size = buffer.Length;
+
+            while (offset < size) //Sigo enviando datos hasta que llegue al final
+            {
+                int sent = _socket.Send(buffer, offset, size - offset, SocketFlags.None);
+                if (sent == 0) //Deja de enviar datos antes qye se envien la cantidad esperada
+                { throw new SocketException(); }
+
+                offset += sent; //La siguiente iteración la empiezo donde terminó la anterior
+            }
+        }
+
+        public byte[] Receive(int size)
+        {
+            byte[] buffer = new byte[size];
+            int offset = 0;
+
+            while (offset < size) //Sigo recibiendo datos hasta completar el array respuesta
+            {
+                int received = _socket.Receive(buffer, offset, size - offset, SocketFlags.None);
+                if (received == 0) //Deja de recibir datos antes de que llegue a la cantidad esperada
+                { throw new SocketException(); }
+
+                offset += received; //La siguiente iteración la empiezo donde terminó la anterior
+            }
+
+            return buffer;
+        }
+    }
+}
